f9c0fde6b3a18ced464f7fef5ffc132bbd511445
Marc Haye <marc.antonio.haye@gmail.com>
2025-08-14 12:17:28 +0200

    feat: update setup for custom configs

--- setup.sh
+++ setup.sh
@@ -20,85 +20,113 @@ BLUE="$(tput setaf 4)"
 SKY_BLUE="$(tput setaf 6)"
 RESET="$(tput sgr0)"
 
-SCRIPT_PATH=$(pwd)
+# Script attributs
+DELAY_BEFORE_REBOOT=5
+
+# Path vars
+RELATIVE_PATH=$(pwd)
+CUSTOM_CONF_PATH="$RELATIVE_PATH/custom-hypr"
+CONF_PATH="~/.config/hypr"
+
+# Jakoolit urls
+$AUTO_INSTALL_URL="https://raw.githubusercontent.com/JaKooLit/Fedora-Hyprland/main/auto-install.sh"
 
 cd ~
+
 # Bash aliases copy
+
 if [ -f .bashrc ]; then
     truncate -s 0 .bashrc
 fi
 
-cat $SCRIPTH_PATH/aliases > .bashrc
+cat $RELATIVE_PATH/aliases > .bashrc
 source .bashrc
 
-# update system & softwares
-echo "${INFO} Updating Fedora... ${RESET}"
+# update system
+echo "${INFO} Updating Fedora..."
+sudo dnf update
 
-u
-i whiptail
-i curl
+# Instal utilities
+sudo dnf install whiptail
+sudo dnf install curl
 
 whiptail --title "Doruo custom Fedora-Hyprland (2025) Install Script" \
     --msgbox "Welcome to Doruo Fedora-Hyprland (2025) Install Script!\n\n\
 Mostly taken from Jakoolit incredible setup: https://github.com/JaKooLit/Fedora-Hyprland"\
     15 80
 
+# Install jakoolit fedora hyprland
+cd ~/Fedora-Hyprland
+sh <(curl -L $AUTO_INSTALL_URL)
+
 # Ask if the user wants to proceed
-if ! whiptail --title "Proceed with Installation?" \
-    --yesno "Would you like to proceed?" 7 50; then
+if ! whiptail --title "Proceed with custom setup?" \
+    --yesno "Would you like to proceed additionnal custom setup?" 7 50; then
     echo -e "\n"
     echo "${INFO} You chose ${YELLOW}NOT${RESET} to proceed. ${YELLOW}Exiting...${RESET}"
     echo -e "\n"
     exit 1
 fi
 
-if whiptail --title "Dev softwares setup." \
-    --yesno "Would you like to install git, pgadmin, postman ?"\n\n\
-    "Mostly taken from Jakoolit incredible setup: https://github.com/JaKooLit/Fedora-Hyprland"\
-7 50; then
+# Ask if the user wants to delete older config
+if whiptail --title "Custom Hyprland config." \
+    --yesno "Would you like to use custom Hyrpland config ?" 7 50; then
+        echo -e "\n"
+        echo "${INFO} You chose to use custom Hyrpland config."
+        echo -e "\n"
+
+        # Custom scripts
+        cp -r $CUSTOM_CONF_PATH/UserScripts $CONF_PATH/UserScripts
+
+        # Custom configs
+        cp -r $CUSTOM_CONF_PATH/configs $CONF_PATH/configs
+
+        # Custom rofi themes
+        cp -r $CUSTOM_CONF_PATH/configs $CONF_PATH/configs
+
+        # Custom wallpapers
+        cp -r $RELATIVE_PATH/wallpapers ~/Pictures/wallpapers
+fi        
+
+if whiptail --title "Dev softwares" \
+    --yesno "Would you like to install dev softwares ?" 7 50; then
         echo -e "\n"
         echo "${INFO} You chose to install softwares. ${YELLOW}Installing...${RESET}"
-        isoftwares        
+
+        # Git
+        sudo dnf install git       
+        # Vscode
+        sudo dnf install code    
+        # Pgadmin4
+        sudo dnf install pgadmin4
+        # Postman
+        sudo dnf install snap
+        sudo snap install postman
 fi  
 
-if whiptail --title "Dev langages setup." \
+if whiptail --title "Dev langages" \
     --yesno "Would you like to install go, python ?" 7 50; then
         echo -e "\n"
         echo "${INFO} You chose to install dev langages (go, python). ${YELLOW}Installing...${RESET}"
-        ilangages     
+        sudo dnf install go    
+        sudo dnf install python3
 fi    
 
-if whiptail --title "Discord install." \
+if whiptail --title "Discord" \
     --yesno "Would you like to install discord ?" 7 50; then
         echo -e "\n"
         echo "${INFO} You chose to install discord. ${YELLOW}Installing...${RESET}"
-        i discord 
+        sudo dnf install discord 
 fi  
-  
-if [ -d "~/Fedora-Hyprland" ]; then
-    # Ask if the user wants to delete older config
-    if whiptail --title "Fedora-Hyprland already exists." \
-        --yesno "Would you like to delete older config ?" 7 50; then
-        echo -e "\n"
-        echo "${INFO} You chose to delete. ${YELLOW}Exiting...${RESET}"
-        echo -e "\n"
-        echo -e "\n ${INFO} Deleting older folder..."
-        rm -rf ~/Fedora-Hyprland
-    fi
-fi
-
-# Install jakoolit fedora hyprland
-cd ~/Fedora-Hyprland
-sh <(curl -L https://raw.githubusercontent.com/JaKooLit/Fedora-Hyprland/main/auto-install.sh)
 
 # Ask if the user wants to reboot
 if whiptail --title "End of Hyprland setup" \
     --yesno "Would you like to reboot?" 7 50; then
     echo -e "\n"
-    echo "${INFO} You chose to reboot. ${YELLOW}Exiting...${RESET}"
+    echo "${INFO} You chose to reboot."
     echo -e "\n"
-    echo -e "${INFO} ${YELLOW}Rebooting your computer...${RESET}"
-    sleep 6
+    echo -e "${INFO} ${YELLOW}Rebooting your computer in $DELAY_BEFORE_REBOOT seconds...${RESET}"
+    sleep $DELAY_BEFORE_REBOOT
     reboot
 fi
 
